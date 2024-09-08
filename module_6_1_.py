d3ec5dd336e9f6c80decb9ff40ce7a0d41d82022
KlodVV <KlodVV@yandex.ru>
2024-09-08 05:57:02 +0300

    Add files via upload

---
+++ module_6_1_.py
@@ -0,0 +1,52 @@
+print(f'Задача - "Съедобное и несъедобное')
+class Animal:
+    alive = True # живой
+    fed = False # накормленный
+    def __init__(self,name):
+        self.name = name
+
+    def eat(self, food):
+        if isinstance(food, Plant):
+            if food.ednible:
+                print(f"{self.name} съел {food.name}")
+                self.fed = True
+            else:
+                print(f"{self.name} не стал есть {food.name}")
+                self.alive = False
+        else:
+            print(f"{self.name} не может есть {food}")
+
+class Mammal (Animal):
+    pass
+
+class Predator (Animal):
+    pass
+class Plant:
+    ednible = False # съедобность
+    def __init__(self,name):
+        self.name = name
+class Flower(Plant):
+    pass
+
+class Fruit(Plant):
+    def __init__(self, name):
+        super().__init__(name)
+        self.ednible = True
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
+
