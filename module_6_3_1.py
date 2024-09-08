d8f5e21435956414cc011fb95b10abe4e027707a
KlodVV <KlodVV@yandex.ru>
2024-09-08 13:27:02 +0300

    Add files via upload

---
+++ module_6_3_1.py
@@ -0,0 +1,53 @@
+print(f'Задача - "Мифическое наследование"')
+class Horse:  # класс описывающий лошадь.
+    sound = 'Frrr'  # - звук, который издаёт лошадь.
+
+    def __init__(self):
+        super().__init__()
+        self.x_distance = 0  # - пройденный путь лошадью.
+
+    def run(self, dx):
+        self.x_distance += dx  # - изменение дистанции, увеличивает x_distance на dx.
+        return self.x_distance
+
+
+class Eagle:  # - класс описывающий орла.
+    sound = 'I train, eat, sleep, and repeat'  # - звук, который издаёт орёл
+
+    def __init__(self):
+        super().__init__()
+        self.y_distance = 0  # - высота полёта
+
+    def fly(self, dy):
+        self.y_distance += dy  # - изменение дистанции орла, увеличивает y_distance на dy.
+        return self.y_distance
+
+
+class Pegasus(Horse, Eagle):  # - класс описывающий пегаса. Наследуется от Horse и Eagle.
+    sound = Eagle.sound
+
+    def __init__(self):
+        super().__init__()
+
+    def move(self, dx, dy):  # - где dx и dy изменения дистанции.
+        return self.fly(dy), self.run(dx)
+
+    def get_pos(self):
+        return self.x_distance, self.y_distance
+
+    def voice(self):
+        print(self.sound)
+
+
+p1 = Pegasus()
+print(p1.get_pos())
+print()
+p1.move(10, 15)
+print(p1.get_pos())
+print()
+p1.move(-5, 20)
+print(p1.get_pos())
+print()
+p1.voice()
+
+print(Pegasus.mro())
