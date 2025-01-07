27f1f29c80a0f0d1320124d23833a80d60e47a5e
vvklod1969 <KlodVV@gmail.com>
2025-01-08 02:03:24 +0500

    Add files via upload

---
+++ products.py
@@ -0,0 +1,26 @@
+products = {
+    "product1": {
+        "name": "Product 1",
+        "description": "Ускоряет сжигание жиров и предотвращает их накопление в организме,\nвыводит токсины, снижает уровень холестерина.\nНа 80% выгоднее зарубежного аналога",
+        "price": 931,
+        "image_url": "https://shop.evalar.ru/upload/iblock/424/ub850grpfux6l7r2wtwhmshpe89xx7oo.png"
+    },
+    "product2": {
+        "name": "Product 2",
+        "description": "Морской хитозан из панцирей крабов для похудения, детокса, контроля уровня холестерина",
+        "price": 859,
+        "image_url": "https://shop.evalar.ru/upload/iblock/843/x95zvl4vqjjioi8ql0crhkxwcz3paddl.png"
+    },
+    "product3": {
+        "name": "Product 3",
+        "description": "Устраняет сразу несколько причин лишнего веса, способствует снижению аппетита, активизирует сжигание жиров",
+        "price": 519,
+        "image_url": "https://shop.evalar.ru/upload/iblock/c51/mam75h042zsrfk7wgmdjhyhtzrugrzua.png"
+    },
+    "product4": {
+        "name": "Product 4",
+        "description": "Cпособствует снижению аппетита, помогает соблюдать диету и контролировать вес",
+        "price": 289,
+        "image_url": "https://shop.evalar.ru/upload/iblock/b70/h3hmu0dit5fqtlghhqcgurb8kwinqtkg.png"
+    }
+}
